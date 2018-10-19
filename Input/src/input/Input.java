be5a50eedf12dc5cab8138ac3424a56e5e00c5c7
gallasam <gallasam@011-4303-S05CN.students.cisd.conroeisd.loc>
2018-10-19 07:49:51 -0500

    Added a print statement to show my change.

--- Input/src/input/Input.java
+++ Input/src/input/Input.java
@@ -35,6 +35,7 @@ public class Input {
           
           out.printf("%s %s %s %s %s", stringArr[0], stringArr[1], stringArr[2], stringArr[3], stringArr[4]);
           
+          System.out.println(".     9 pump headshot. <33");
           
           
           
